20b67539f833cce53060d198403ce4ba2ee1107e
justoestevez <jeestevez@mail.austral.edu.ar>
2026-03-31 09:45:05 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -19,13 +19,13 @@ def grades():
         Insuficiente
     """
     nota = float(input())
-
-     if nota >= 9 and nota <= 10:
-         print("Excelente")
-     elif nota >= 7 and nota <= 8:
-         print("Bueno")
-     elif nota >= 5 and nota <= 6:
-         print("Regular")
-     elif nota >= 0 and nota <= 4:
-         print("Insuficiente") 
+    if nota >= 9 and nota <= 10:
+        print("Excelente")
+    elif nota >= 7 and nota <= 8:
+        print("Bueno")
+    elif nota >= 5 and nota <= 6:
+        print("Regular")
+    elif nota >= 0 and nota <= 4:
+        print("Insuficiente") 
+ 
     
